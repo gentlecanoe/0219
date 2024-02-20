299c000defd5948ff41c1696a12a969707560c1c
gentlecanoe <139728217+gentlecanoe@users.noreply.github.com>
2024-02-20 14:43:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"]
+@app.route("/main",methods=["GET","POST"])
 def main():
   name = requset.form.get("name")
    return(render_template("main.html",r=name))
